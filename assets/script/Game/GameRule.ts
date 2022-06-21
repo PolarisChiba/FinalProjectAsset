f22f69bdffff70d0766c4f373f0f971d96c113a5
109062105 <james0922707696@gmail.com>
2022-06-21 10:23:10 +0800

    fix bugs, add rules

---
+++ assets/script/Game/GameRule.ts
@@ -0,0 +1,26 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, this.Pushed, this);
+    }
+
+    // update (dt) {}
+
+    Pushed() {
+        cc.director.loadScene("Room");
+    }
+}
